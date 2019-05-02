4aa7aef245dcff65ed9fc91841de57a7a2f6b50f
omaslov <omaslov@xbsoftware.by>
2019-05-02 11:48:15 +0300

    add img to popup

--- popup.js
+++ popup.js
@@ -16,15 +16,18 @@ document.addEventListener("DOMContentLoaded", function() {
 		that.btn = obj.btn;
 		that.overlay = obj.overlay;
 		that.popup = obj.popup;
+		that.content = obj.content;
 		function showPopup (e) {
 			document.querySelector(that.overlay).classList.toggle('active');
 			document.querySelector(that.popup).classList.toggle('active');
+			document.querySelector(that.popup).innerHTML = that.content;
 		}
 		document.querySelector(that.btn).addEventListener('click', showPopup);
 		document.querySelector(that.overlay).addEventListener('click', showPopup);
 	}
 	// Потом надо сделать это по приницпам ООП
 	var popup = new PopUp({
+		content:'<h1>Hello</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, maiores!</p><img class="popup-img" src="img/Developer-woman.svg">',
 		btn: '#showPopup',
 		overlay: '.overlay',
 		popup: '#popup',
